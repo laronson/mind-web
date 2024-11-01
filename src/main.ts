852e4c12bab3c021b252180b1e0cc606bff980d5
laronson <laronson21@gmail.com>
2024-11-01 16:27:58 -0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+import { app, BrowserWindow, globalShortcut } from 'electron';
+import path from 'path';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+
+
+  // Register a global shortcut listener.
+  const ret = globalShortcut.register('CommandOrControl+N', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  if (!ret) {
+    console.log('Global shortcut registration failed.');
+  }
+
+  // Hide the window instead of closing it.
+  mainWindow.on('close', (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', () => {
+  createWindow()
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
